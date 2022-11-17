436deae57710eef18a39d91b6a486d6e945753b4
smirnov <smirnov@flagstudio.ru>
2022-11-17 17:13:32 +0500

    WIP

--- app/Actions/FilterProduct.php
+++ app/Actions/FilterProduct.php
@@ -4,22 +4,32 @@ namespace App\Actions;
 
 use App\DTO\FilterProductDTO;
 use App\Models\Product;
+use MeiliSearch\Endpoints\Indexes;
 
 class FilterProduct
 {
     public function handle(FilterProductDTO $data)
     {
-        return Product::with('colors')
-            ->when($data->colors, function ($query) use ($data) {
-                $query->whereHas('colors', function ($query) use ($data) {
-                    $query->whereIn('colors.id', $data->colors);
-                });
-            })
-            ->when($data->category, function ($query) use ($data) {
-                $query->whereCategoryId($data->category);
-            })
-            ->where('price', '>=', $data->priceMin)
-            ->where('price', '<=', $data->priceMax)
+        return Product::search(
+            '',
+//            function(Indexes $meiliSearch, string $query, array $options) use ($data) {
+//                $options['sort'] = ['id:asc'];
+//
+//                return $meiliSearch->search($query, $options);
+//            }
+        )
+//            ->query()
+//            ->with('colors')
+//            ->when($data->colors, function ($query) use ($data) {
+//                $query->whereHas('colors', function ($query) use ($data) {
+//                    $query->whereIn('colors.id', $data->colors);
+//                });
+//            })
+//            ->when($data->category, function ($query) use ($data) {
+//                $query->whereCategoryId($data->category);
+//            })
+//            ->where('price', '>=', $data->priceMin)
+//            ->where('price', '<=', $data->priceMax)
             ->paginate();
     }
 }
